0be8762bf4253bbc15aa07ed53c60f4acc0bdc47
Scott <scott.tolinski@gmail.com>
2018-03-03 17:30:47 -0700

    moves video to bg

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,30 +6,34 @@ import 'typeface-lato';
 import 'typeface-inconsolata';
 import { rhythm } from '../utils/typography';
 
-import { SiteHeader, SiteNav, NavLink, SubHeader, Logo, Name, Main } from '../components/Headings';
-import {
-  SocialLinks,
-  SocialLink,
-} from '../components/Footer';
+import { SiteHeader, SiteNav, NavLink, SubHeader, Logo, Name, Main, BillboardVideo } from '../components/Headings';
+import { SocialLinks, SocialLink } from '../components/Footer';
+
+import billboardmp4 from '../pages/showcase2.mp4';
+import billboardwebm from '../pages/showcase2.webmhd.webm';
 
 const FooterLinks = [
   {
     name: 'Twitter',
     link: 'https://twitter.com/stolinski',
     color: '#00aced',
-  }, {
+  },
+  {
     name: 'Youtube',
     link: 'http://www.youtube.com/user/LevelUpTuts',
     color: '#b00',
-  }, {
+  },
+  {
     name: 'Github',
     link: 'https://github.com/stolinski',
     color: '#4183c4',
-  }, {
+  },
+  {
     name: 'BitBucket',
     link: 'https://bitbucket.org/stolinski',
     color: '#205081',
-  }, {
+  },
+  {
     name: 'Facebook',
     link: 'https://www.facebook.com/LevelUpTuts',
     color: '#3b5998',
@@ -39,60 +43,57 @@ const FooterLinks = [
 export default class Template extends React.Component {
   static propTypes = {
     children: PropTypes.func,
-  }
+  };
 
   render() {
     return (
       <div>
         <Helmet
           title="Scott Tolinski"
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
+          meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
         />
-
+        <BillboardVideo>
+          <source src={billboardmp4} type="video/mp4" />
+          <source src={billboardwebm} type="video/webm" />
+        </BillboardVideo>
         <SiteHeader>
           <Link to="/">
-            <Logo page={this.props.location.pathname.replace('/', '')}>
-              ST
-            </Logo>
+            <Logo page={this.props.location.pathname.replace('/', '')}>ST</Logo>
             <Name>Scott Tolinski</Name>
           </Link>
           <SiteNav>
             <ul>
               <li>
-                <NavLink underline="#16a085" to="/about">About</NavLink>
+                <NavLink underline="#16a085" to="/about">
+                  About
+                </NavLink>
               </li>
               <li>
-                <NavLink underline="#e74c3c" to="/projects">Projects</NavLink>
+                <NavLink underline="#e74c3c" to="/projects">
+                  Projects
+                </NavLink>
               </li>
               <li>
-                <NavLink underline="#2980b9" to="/contact">Contact</NavLink>
+                <NavLink underline="#2980b9" to="/contact">
+                  Contact
+                </NavLink>
               </li>
             </ul>
           </SiteNav>
         </SiteHeader>
 
-        <Main>
-          {this.props.children()}
-        </Main>
+        <Main>{this.props.children()}</Main>
 
         <footer className="footer">
           <SubHeader>Where to find me</SubHeader>
           <SocialLinks>
             {FooterLinks.map(link => (
               <li key={link.name}>
-                <SocialLink
-                  target="_blank"
-                  rel="nofollow"
-                  color={link.color}
-                  href={link.link}
-                >
+                <SocialLink target="_blank" rel="nofollow" color={link.color} href={link.link}>
                   {link.name}
                 </SocialLink>
               </li>
-          ))}
+            ))}
           </SocialLinks>
         </footer>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 
-import billboardmp4 from './showcase2.mp4';
-import billboardwebm from './showcase2.webmhd.webm';
-
-import { SiteTitle, BillboardVideo, MainWrapper } from '../components/Headings';
+import { SiteTitle, MainWrapper } from '../components/Headings';
 
 export default class Index extends Component {
   render() {
     return (
       <div>
-        <BillboardVideo>
-          <source src={billboardmp4} type="video/mp4" />
-          <source src={billboardwebm} type="video/webm" />
-        </BillboardVideo>
         <MainWrapper>
           <SiteTitle>I am Scott Tolinski</SiteTitle>
           <p>I make excellent, modern websites.</p>
