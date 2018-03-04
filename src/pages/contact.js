0c4d05783200ebb08afff25abb0c0041af27e604
Scott <scott.tolinski@gmail.com>
2018-03-03 17:51:01 -0700

    adds contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,32 @@ export default class Index extends React.Component {
     return (
       <MainWrapper color="#2980b9">
         <Title>Contact</Title>
-        <p>Feel free to contact me through the email below, I'd love to chat.</p><p className="cont"><a href="mailto:scott.tolinski@gmail.com" className="email">scott.tolinski@gmail.com</a></p>
+        <form name="contact" method="POST" netlify>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+        <p>Feel free to contact me through the email below, I'd love to chat.</p>
+        <p className="cont">
+          <a href="mailto:scott.tolinski@gmail.com" className="email">
+            scott.tolinski@gmail.com
+          </a>
+        </p>
       </MainWrapper>
     );
   }
