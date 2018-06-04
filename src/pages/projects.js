c9c8e3a56db299b5fc02f2ea9de95bb3371a6159
Ankit Tiwari <ankitt255@gmail.com>
2018-06-04 22:22:08 +0530

    📝Add rel="nofollow" in anchor tags

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -16,19 +16,19 @@ const projects = [
   }, {
     title: 'Level Up Tutorials',
     link: 'https://www.leveluptutorials.com/',
-    desc: '1000+ Web Dev Tutorials on: <a target="_blank" href="https://www.youtube.com/channel/UCyU5wkjgQYGRB0hIHMwm2Sg">Youtube</a><br />Built With: Meteor, React, Stylus',
+    desc: '1000+ Web Dev Tutorials on: <a target="_blank" rel="nofollow" href="https://www.youtube.com/channel/UCyU5wkjgQYGRB0hIHMwm2Sg">Youtube</a><br />Built With: Meteor, React, Stylus',
   }, {
     title: 'Q LTD',
     link: 'http://qltd.com/',
-    desc: 'Built for: <a target="_blank" href="http://qltd.com">Q LTD</a><br />Built With: Node.js, Express, MongoDB, Sass/Compass/Susy',
+    desc: 'Built for: <a target="_blank" rel="nofollow" href="http://qltd.com">Q LTD</a><br />Built With: Node.js, Express, MongoDB, Sass/Compass/Susy',
   }, {
     title: 'ACM SIGGRAPH',
     link: 'http://www.siggraph.org/',
-    desc: 'Built for: <a target="_blank" href="http://qltd.com">Q LTD</a><br />Built With: Drupal 7, Omega 3, Sass/Compass',
+    desc: 'Built for: <a target="_blank" rel="nofollow" href="http://qltd.com">Q LTD</a><br />Built With: Drupal 7, Omega 3, Sass/Compass',
   }, {
     title: 'Concrete.org',
     link: 'http://www.concrete.org/',
-    desc: 'Built for: <a target="_blank" href="http://qltd.com">Q LTD</a><br />Built Templates With: HTML5, CSS3, JavaScript',
+    desc: 'Built for: <a target="_blank" rel="nofollow" href="http://qltd.com">Q LTD</a><br />Built Templates With: HTML5, CSS3, JavaScript',
   },
 ];
 
@@ -43,7 +43,7 @@ export default class Projects extends React.Component {
           {projects.map(project => (
             <li>
               <h3 className="project-title">
-                <a target="_blank" href={project.link}>{project.title}</a>
+                <a target="_blank" rel="nofollow" href={project.link}>{project.title}</a>
               </h3>
               <p dangerouslySetInnerHTML={{ __html: project.desc }} />
             </li>
